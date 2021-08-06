edb084fed14fd020d402821df2aef38a94493dd8
kukuvz <antonslayer@gmail.com>
2021-08-06 15:13:03 +0300

    js changes

--- app/js/main.js
+++ app/js/main.js
@@ -93,6 +93,7 @@ $(function(){
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="arrows-right"></img>',
     });
 
+    //input and plus minus buttons in sleep block
     $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="/img/plus.svg" alt="plus"></div><div class="quantity-button quantity-down"><img src="/img/minus.svg" alt="minus"></div></div>').insertAfter('.quantity input');
     $('.quantity').each(function() {
       var spinner = $(this),
@@ -102,7 +103,7 @@ $(function(){
         min = input.attr('min'),
         max = input.attr('max');
 
-      btnUp.click(function() {
+      btnUp.on("click", function() {
         var oldValue = parseFloat(input.val());
         if (oldValue >= max) {
           var newVal = oldValue;
@@ -113,7 +114,7 @@ $(function(){
         spinner.find("input").trigger("change");
       });
 
-      btnDown.click(function() {
+      btnDown.on("click", function() {
         var oldValue = parseFloat(input.val());
         if (oldValue <= min) {
           var newVal = oldValue;
@@ -126,22 +127,80 @@ $(function(){
 
     });
 
+    //counting accommodation in sleep block
     $('.quantity-button').on('click', function() {
         let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
         $('.summ').html('$' + summ);
     });
 
-    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
-    $('.summ').html('$' + summ);
-
     $('.surfboard-box__circle').on('click', function() {
       $(this).toggleClass('active');
     });
+
 
     $('.menu-btn').on('click', function() {
         $('.menu').toggleClass('active');
     });
 
+    //init wow.js
     new WOW().init();
-  
+
+    //Smooth scroll and pageup
+    $(window).on("scroll",function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").on('click', function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+
+    //date on main page
+    function getDateYear() {
+        const d = new Date(),
+              day = d.getDate(),
+              month = d.getMonth(),
+              year = d.getFullYear();
+            
+            return {
+                'days': day,
+                'months': month,
+                'years': year
+            };
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock() {
+        const days = document.querySelector(".header__date-day"),
+              monthYear = document.querySelector('.header__month-year');
+
+        updateClock();
+
+        function updateClock() {
+            const t = getDateYear();
+
+            days.innerHTML = getZero(t.days);
+            monthYear.innerHTML = `${getZero(t.months + 1)} | ${getZero(t.years)}`;
+        }
+    }
+
+    setClock();
+    
 });
+
+
+const now = new Date();
+
+console.log(now.getMonth());
